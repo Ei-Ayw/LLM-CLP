baa14a0c7036c38d6fd16aec9caa771487e7f621
echo_Eire <zheng1_9@qq.com>
2026-05-16 17:22:47 +0800

    commit

--- src/counterfactual/__init__.py
+++ src/counterfactual/__init__.py
@@ -8,6 +8,20 @@ from src.counterfactual.prompts import (
     GROUP_KEYWORDS,
 )
 from src.counterfactual.schema import CounterfactualPair
+from src.counterfactual.generator_llm import (
+    ZhipuGenerator,
+    ZhipuGeneratorPool,
+    OpenAICompatGenerator,
+    generate_counterfactuals_for_dataset,
+)
+from src.counterfactual.generator_swap import (
+    generate_swap_counterfactuals,
+    batch_generate_swap,
+)
+from src.counterfactual.validator import (
+    validate_counterfactual,
+    validate_and_filter,
+)
 
 __all__ = [
     "PROMPT_FIXED_MAPPING",
@@ -15,4 +29,15 @@ __all__ = [
     "IDENTITY_SWAP_PAIRS",
     "GROUP_KEYWORDS",
     "CounterfactualPair",
+    # LLM Generators
+    "ZhipuGenerator",
+    "ZhipuGeneratorPool",
+    "OpenAICompatGenerator",
+    "generate_counterfactuals_for_dataset",
+    # Swap Generators
+    "generate_swap_counterfactuals",
+    "batch_generate_swap",
+    # Validators
+    "validate_counterfactual",
+    "validate_and_filter",
 ]
